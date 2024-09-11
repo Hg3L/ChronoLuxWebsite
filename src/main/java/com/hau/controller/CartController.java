e001c28b60553bbf66a845274a7eb53d1b96cd88
Hoang Lam <HgL03.working@gmail.com>
2024-09-11 18:06:55 +0700

    Add contact, product-detail, cart, checkout

---
+++ src/main/java/com/hau/controller/CartController.java
@@ -0,0 +1,15 @@
+package com.hau.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CartController {
+    @RequestMapping(value = "/cart", method = RequestMethod.GET)
+    public ModelAndView cartPage() {
+        ModelAndView mav = new ModelAndView("web/cart");
+        return mav;
+    }
+}
