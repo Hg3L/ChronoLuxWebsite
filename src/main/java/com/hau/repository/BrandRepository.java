abdb8b6493646c94a70df600da27033319ed766d
dhuycoder <lhuy123hl@gmail>
2024-09-25 20:50:17 +0700

    Thêm các Entity, Hiển thị brand ra View Home

---
+++ src/main/java/com/hau/repository/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.hau.repository;
+
+import com.hau.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+
+}
