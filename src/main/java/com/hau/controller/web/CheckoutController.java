c2c25ddb4cdc85f99bd4159e811f5f9cea81c673
dhuycoder <lhuy123hl@gmail.com>
2024-10-22 12:08:59 +0700

    Thêm chức năng hiển thị cho checkout-success,checkout-review

--- src/main/java/com/hau/controller/web/CheckoutController.java
+++ src/main/java/com/hau/controller/web/CheckoutController.java
@@ -3,10 +3,19 @@ package com.hau.controller.web;
 import com.hau.dto.UserDTO;
 import com.hau.service.CartItemService;
 import com.hau.service.UserService;
+import com.hau.service.impl.PaymentServices;
+import com.paypal.api.payments.PayerInfo;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.ShippingAddress;
+import com.paypal.api.payments.Transaction;
+import com.paypal.base.rest.PayPalRESTException;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,4 +40,36 @@ public class CheckoutController {
         mav.addObject("cartItems",  cartItemService.findAllByUser(userDTO));
         return mav;
     }
+
+    @GetMapping("/checkout/review")
+    public ModelAndView ReviewPayment(@RequestParam("paymentId") String paymentId,
+                                      @RequestParam("PayerID") String payerId){
+        ModelAndView mav = new ModelAndView();
+        try{
+            PaymentServices paymentServices = new PaymentServices();
+            Payment payment = paymentServices.getPaymentDetails(paymentId);
+            PayerInfo payerInfo = payment.getPayer().getPayerInfo();
+            Transaction transaction = payment.getTransactions().get(0);
+            ShippingAddress shippingAddress = transaction.getItemList().getShippingAddress();
+            mav = new ModelAndView("web/checkout-review");
+            mav.addObject("paymentId",paymentId);
+            mav.addObject("payerId",payerId);
+            mav.addObject("payer",payerInfo);
+            mav.addObject("transaction",transaction);
+            mav.addObject("shippingAddress",shippingAddress);
+
+
+        }catch (PayPalRESTException ex){
+                ex.printStackTrace();
+                mav = new ModelAndView("web/checkout");
+                mav.addObject("error","Could not get payment details");
+        }
+        return mav;
+    }
+
+    @GetMapping("/checkout/success")
+    public String CheckoutSuccess(){
+        return "web/checkout-success";
+    }
+
 }
